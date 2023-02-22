5c955937f6ec04bea4b959396fb96571ecc5dc7e
mickstraathof <mick.straathof@hotmail.com>
2023-02-22 19:59:17 +0100

    logo en huisstijlen toegevoegd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,24 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="logo-container">
+    <div class="logo">
+      <div class="inner-logo">
+
+      </div>
+    </div>
+    <div class="logo-text">
+      <h1>Real Estate Care</h1>
+      <p>Caring is in our nature</p>
+    </div>
+  </div>
+
+  <div class="left-bubble">
+
+  </div>
+
+  <div class="right-bubble">
+    <div class="inner-bubble">
+
+    </div>
   </div>
 </template>
 
@@ -40,19 +32,72 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .logo-container{
+    width: fit-content;
+    display: flex;
+    gap: 2em;
+    position: absolute;
+    top: 3em;
+    left: 3em;
+    z-index: 1;
+  }
+
+  .logo{
+    width: 8dvh;
+    height: 8dvh;
+    border: .3em solid black;
+    /*position: absolute;*/
+    /*top: 2em;*/
+    /*left: 2em;*/
+    transform: rotateY(0deg) rotate(45deg);
+    position: relative;
+  }
+
+  .inner-logo{
+    float: left;
+    width: 30%;
+    height: 30%;
+    position: absolute;
+    bottom: 15%;
+    right: 15%;
+    border-top: .3em solid black;
+    border-left: .3em solid black;
+  }
+
+  .logo-text{
+    float: right;
+    font-family: monospace;
+    font-size: 1.8em;
+  }
+
+  .left-bubble{
+    position: absolute;
+    bottom: -10%;
+    left: 0;
+    height: 20dvh;
+    width: 50dvh;
+    background-color: rgb(0, 180, 162);
+    border-top-right-radius: 100% 100%;
+  }
+
+  .right-bubble{
+    position: absolute;
+    top: -10%;
+    right: 0;
+    height: 30dvh;
+    width: 80dvh;
+    border-bottom-left-radius: 100% 100%;
+    background-color: rgb(0, 180, 162);
+  }
+
+  .inner-bubble{
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 27dvh;
+    width: 65dvh;
+    border-bottom-left-radius: 100% 100%;
+    background-color: rgb(20, 27, 31);
+  }
 </style>
