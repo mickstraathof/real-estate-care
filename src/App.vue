1f1aa948a98573f8f8ca7b35cf95f98c1af3ba9f
Mick <mick.straathof@hotmail.com>
2023-02-25 15:25:46 +0100

    created layout with footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
 <!--  <HelloWorld />-->
   <Header />
+    <p><!-- temp filling --></p>
+  <Footer />
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    Header
+    Header,
+    Footer
   }
 }
 </script>
@@ -20,5 +24,12 @@ export default {
 * {
   margin: 0;
   padding: 0;
+}
+
+#app{
+  min-height: 100vh;
+  margin: 0;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-items>
+      <v-btn>Dashboard</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Footer-component'
+}
+</script>
+
+<style>
+
+
+
+</style>
