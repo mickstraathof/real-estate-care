8deaa11e7a83ef9a37a6290625ca2051c0493731
mickstraathof <mick.straathof@hotmail.com>
2023-03-12 17:15:25 +0100

    Update CompletedReport.vue

--- src/views/CompletedReport.vue
+++ src/views/CompletedReport.vue
@@ -1,11 +1,18 @@
 <template>
-    //
+    <RightBubble />
+    <LeftBubble />
 </template>
 
 
 <script>
+    //import components
+    import LeftBubble from '@/components/LeftBubble.vue';
+    import RightBubble from '@/components/RightBubble.vue';
     export default{
-        
+        components: {
+            LeftBubble,
+            RightBubble,
+        }
     }
 </script>
 
