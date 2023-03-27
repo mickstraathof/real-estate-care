088f59b5021a642b9e6f0fd28e23c076bb6b71b9
Mick <mick.straathof@hotmail.com>
2023-03-27 11:24:55 +0200

    added settings view

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import CompletedReports from "../views/CompletedReports";
 import CompletedReport from "../views/CompletedReport";
 import AssignedReport from "../views/AssignedReport";
 import InfoView from "../views/InfoView";
+import SettingsView from "../views/SettingsView";
 
 const routes = [
     //declaring routes
@@ -39,6 +40,11 @@ const routes = [
         path: '/info',
         name: 'info',
         component: InfoView,
+    },
+    {
+        path: '/settings',
+        name: 'settings',
+        component: SettingsView,
     }
 ]
 
